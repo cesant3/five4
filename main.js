50744ec22877f892f17e1eaa6576aa0402988445
Carlos Santiago <cesantiago3@gmail.com>
2021-08-10 18:09:24 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const menuBtn = document.querySelector(".burger-menu");
+let menuOpen = document.getElementById("menu");
+
+menuBtn.addEventListener("click", () => {
+  if (menuOpen.style.display === "flex") {
+    menuOpen.style.display = "none"
+  } else {
+    menuOpen.style.display = "flex"
+  }
+});
